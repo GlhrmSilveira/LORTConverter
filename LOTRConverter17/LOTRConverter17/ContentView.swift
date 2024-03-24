80a50782cb26fd2a051a7b3e28b3a89071d82c67
gui silveira <gssouza@uolinc.com>
2024-03-24 18:11:48 -0300

    GUI - Beggin Add Images Views and Text Views

--- LOTRConverter17/LOTRConverter17/ContentView.swift
+++ LOTRConverter17/LOTRConverter17/ContentView.swift
@@ -11,11 +11,21 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             //Background Image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
             
             VStack {
                 // Prancing Pony Image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
                 
                 // Currency Exchange Text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
                 
                 // Currency Conversion Section
                 HStack {
@@ -24,30 +34,56 @@ struct ContentView: View {
                         // Currency
                         HStack {
                             // Currency Image
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                             
                             // Currency Text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                         }
                         
                         // Textfield
+                        Text("Textfield")
                     }
                     
                     // Equal Sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
                     
                     // Rigth Conversion Section
                     VStack {
                         // Currency
                         HStack {
-                            // Currency Image
-                            
                             // Currency Text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            
+                            // Currency Image
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                         }
                         
                         // Textfield
+                        Text("Textfield")
                     }
                 }
                 
+                Spacer()
+                
                 // Info Button
+                Image(systemName: "info.circle.fill")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
             }
+//            .border(.blue)
         }
     }
 }
