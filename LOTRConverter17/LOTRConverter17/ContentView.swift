fd9dcfafe68eda406886215f3c329a9956852b5e
gui silveira <gssouza@uolinc.com>
2024-03-24 17:17:47 -0300

    GUI - Layout of  my MainView with ZSTACK, VSTACK and HSTACK

---
+++ LOTRConverter17/LOTRConverter17/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  LOTRConverter17
+//
+//  Created by Guilherme Silveira de Souza on 24/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            //Background Image
+            
+            VStack {
+                // Prancing Pony Image
+                
+                // Currency Exchange Text
+                
+                // Currency Conversion Section
+                HStack {
+                    // Left Conversion Section
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Currency Image
+                            
+                            // Currency Text
+                        }
+                        
+                        // Textfield
+                    }
+                    
+                    // Equal Sign
+                    
+                    // Rigth Conversion Section
+                    VStack {
+                        // Currency
+                        HStack {
+                            // Currency Image
+                            
+                            // Currency Text
+                        }
+                        
+                        // Textfield
+                    }
+                }
+                
+                // Info Button
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
